3be03d38a906a1c294806dd41d682d977ef484a0
Yashkhatrii6 <yashkhatri3344@gmail.com>
2025-02-19 10:53:46 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,9 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Check if they have common elements
+common_elements = set1 & set2
+if common_elements:
+    print("Common elements:", common_elements)
+else:
+    print("No common elements")
